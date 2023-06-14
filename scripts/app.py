df09c6aadc17e930acef78d028d9eb8a7c32937c
Alexander Belski <sascha.belski@gmail.com>
2023-06-14 17:16:05 +0200

    Code added

--- scripts/app.py
+++ scripts/app.py
@@ -1,36 +1,63 @@
 import streamlit as st
 from streamlit_image_select import image_select
 import os
+from PIL import Image
 
-from streamlit_modal import Modal
+# from streamlit_modal import Modal
+from io import StringIO
+from streamlit.components.v1 import html
 
 from streamlit_option_menu import option_menu
 
+import extra_streamlit_components as stx
 
-from streamlit.components.v1 import html
+from  prediction import artpredictor, load_da_model
+from apis import *
 
-if "selected_tab" not in st.session_state:
-        st.session_state.selected_tab = 0
+#session vars
+if 'tab_nr' not in st.session_state:
+    st.session_state['tab_nr'] = 1
+if 'img' not in st.session_state:
+    st.session_state['img'] = None
+if 'imgart' not in st.session_state:
+    st.session_state['imgart'] = 3
+
+# with st.spinner('Loading model in progress - wait for it...'):
+model =  load_da_model()
     
 #sidebar
-st.sidebar.title("Menu")
+st.sidebar.markdown(
+    """
+    <style>
+         [data-testid=stSidebar] [data-testid=stImage] > img{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            --margin-right: auto;
+            width: 80%;
+        }
+        [data-testid=stSidebar] [data-testid=stMarkdownContainer] > div > h1{
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
+st.sidebar.markdown(
+    """
+    <style>
+        [data-testid=column] {background-color: rgba(232,242,252,255);}
+    </style>
+    """, unsafe_allow_html=True
+)
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Artist prediction', "Methodology", "Art generation", "Music generation"], 
+    st.sidebar.image("https://lewagon.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Ffe03270b-e175-406a-a223-7d362fb51bd4%2Fnest.png?table=block&id=5590833d-acb5-43ac-ba73-353ac22399d7&spaceId=2b6dea13-478f-4f00-aff1-afd11ed5a03f&width=250&userId=&cache=v2", use_column_width=False)
+    st.sidebar.title("Artist prediction")
+    selected = option_menu("Main Menu", ["Home", 'Artist prediction', "Methodology", "Art generation", "Music generation", "References"], 
         icons=['house', 'broadcast'], menu_icon="cast", default_index=1)
     masel = selected
-'''
-
-'''
-# def callmodal(pimg):
-#     modal = Modal(key="Demo Key",title="selected image")
-#     open_modal = st.button(key="123", label='button')
-#     #open_modal = pimg is not None
-#     if open_modal:
-#         with modal.container():
-#             st.markdown(st.image(pimg))
-# show images
-
-# blank html to be used later 
 
 # Define your javascript
 my_js = """
@@ -38,51 +65,137 @@ alert("Hola mundo");
 """
 
 # Wrapt the javascript as html code
-my_html = """
-
-"""
+my_html = f"<script>{my_js}</script>"
 
 st.write(masel)
 
 if masel == 'Home':
+   tab10, tab11 = st.tabs(["Artist prediction", "About"])
    with st.container():
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    with tab10:
+        st.write("Creation of art is among the highest form of expression of human mind and imagination. The ability of communicating imagination sets us apart from all other beings. Painting, being an expression of visual language, have attracted and connected the brilliant human minds since the dawn of civilization - from early drawings on walls of caves to paper or glass paintings of modern times, from charcoals in prehistoric times to water, oil, or pastel colors of today. We have travelled a long way, and have finally reached a stage where not only humans but computers, another brilliant creation of human minds, is creating paintings.")
+        st.write("For an enthusiast of arts, identifying the paintings of her favorite artists is not that difficult, given years of careful practice and research. Given a painting, she can easily identify if it was painted by a painter she is passionate about. But can a computer do the same? Can a machine without emotions identify who the genius is behind a mindblowing painting?")
+        st.write("In this part of the project, let us try to explore that direction, using techniques of deep learning.")
+  
+if masel == 'Methodology':
+    tab31, tab32, tab33 = st.tabs(["Convolutional neural network", "Transfer learning", "Variational Autoencoder"])
+    with st.container():      
+        with tab31:
+            st.write("In deep learning, a convolutional neural network (CNN) is a class of artificial neural network most commonly applied to analyze visual imagery. CNNs use a mathematical operation called convolution in place of general matrix multiplication in at least one of their layers. They are specifically designed to process pixel data and are used in image recognition and processing.")
+        with tab32:
+            st.write("Transfer learning (TL) is a research problem in machine learning (ML) that focuses on applying knowledge gained while solving one task to a related task. For example, knowledge gained while learning to recognize cars could be applied when trying to recognize trucks. This topic is related to the psychological literature on transfer of learning, although practical ties between the two fields are limited. Reusing/transferring information from previously learned tasks to new tasks has the potential to significantly improve learning efficiency.")
+        with tab33:
+            st.write("Variational autoencoders are probabilistic generative models that require neural networks as only a part of their overall structure. The neural network components are typically referred to as the encoder and decoder for the first and second component respectively.")
 
 if masel == 'Artist prediction':
-    tab1, tab2 = st.tabs(["Pictures", "Predictions"])
-    #selected_tab = st.radio("Select a tab", st.tabs, index=st.session_state.selected_tab)
-
-    # Update the selected tab index
-    #st.session_state.selected_tab = st.tabs.index(selected_tab)
     
-    with tab1:
-        tab1.subheader("Choosing data for prediction")
+    if st.session_state['tab_nr'] == 1:
+        st.info("Art gallery: please choose a masterpiece in the gallery for the prediction")
+        
         with st.container():
-            #if st.button('predict the author'):
-                # routine for the prediction
-            st.button("redict the author", on_click=lambda: st.markdown("Switching to Tab 2...") or st.experimental_rerun() or st.markdown("Tab 2 content"))
+            if st.button('predict the artist'):
+                st.session_state['tab_nr'] = 2
+                st.experimental_rerun()
+        # if not (st.session_state['img'] is None):
+        st.info("selected picture")
+        col01, col02 = st.columns(2)
+        with col01:  
+            imgcontainer = st.container()
+        with col02:
+            lblcontainer = st.container()
+            imgplaceholder = st.image
+        
+        with st.expander("Sselect a picture from the url", expanded=False):
+            st.info("please paste the url in the field below")
+            img1 = st.text_input('Artwork url')
+            st.write('the selected masterpiece url is', img1)
+            if (img1 is not None) and  (img1 != ""):
+                st.info(img1)
+                st.session_state['img'] = img1
+                st.session_state['imgart'] = 1
+                with lblcontainer:
+                    st.write(f"url from internet{img1}")
+                with imgcontainer:
+                    imgplaceholder(st.session_state['img'])
             
-        with st.expander("Sselect a picture to predict", expanded=True):
+        #with st.expander("Sselect a picture from the hard drive", expanded=False):
+            # st.info("firstselect picture from your hard drive")
+            # uploaded_file = st.file_uploader("Choose a file")   
+            # st.write(uploaded_file)
+            # st.session_state['img'] = uploaded_file
+            # # st.session_state['img'] = img
+            # st.session_state['imgart'] = 2
+            #st.write(st.session_state['img'])
+            # with lblcontainer:
+            #      st.write(st.session_state['img'])
+            # with imgcontainer:
+            #      st.image(st.session_state['img'], width=200) 
+                 
+        with st.expander("Select a picture to predict from the gallery", expanded=True):
             images = []
-            for image in os.listdir("../melodic_canvases_project/data/images"):
+            for image in os.listdir("./melodic_canvases_project/data/images"):
                 if image.endswith(".jpg"):
-                    images.append(f"../melodic_canvases_project/data/images/{image}")
+                    images.append(f"./melodic_canvases_project/data/images/{image}")
                 else:
                     continue
-            img = image_select(
+            img3 = image_select(
             label="Select a picture",
             images = images,
             )
-            st.write(img)
+            if st.session_state['imgart'] != 1:
+                if (img3 is not None) and  (img3 != ""):
+                    st.session_state['img'] = img3
+                    st.session_state['imgart'] = 3
+                    #st.write(st.session_state['img'])
+                    with lblcontainer:
+                        st.write(st.session_state['img'].split('/')[-1].replace('_', ' '))
+                    with imgcontainer:
+                        imgplaceholder(st.session_state['img'], width=200)     
 
-    with tab2:
-        tab2.subheader("Prediction")
-        st.write("selected picture")
-        st.image(img, width=600)
+    if st.session_state['tab_nr'] == 2:
+        st.info("Prediction")
+        with st.container():
+            if st.button('back to the gallery'):
+                st.session_state['tab_nr'] = 1
+                st.experimental_rerun()
+         
+        col1, col2 = st.columns(2)
+        with col1:
+            with  st.container():      
+                st.info("selected picture")
+                st.image(st.session_state['img'], width=300)
 
-if masel == 'Methodology':
+        # prediction
+        with col2:
+            predictcontainer = st.container()
+        
+        with st.expander("Autobiography", expanded=True):
+            apiautobiography = st.container()
+        
+        with st.spinner('Prediction in progress - wait for it...'):
+            # model =  load_da_model()
+            artist, probability = artpredictor(model, st.session_state['img'])
+            # st.info(f"model-predicted artist: {artist},  prediction probability:{probability}")
+            # apis info
+            abiography, aimage_url = get_artist_info(artist)
+            aquote = get_random_quote_by_person(artist)
+            with predictcontainer:
+                st.info("prediction results:")
+                st.success(f"Predicted Artist: {artist}  \nPrediction Probability: {probability}")
+                st.info(f"original label: {st.session_state['img'].split('/')[-1].replace('_', ' ')}")
+            with apiautobiography:
+                st.info(artist)
+                col21, col22 = st.columns(spec= [0.2, 0.8], gap="small")
+                with col21:
+                    st.image(aimage_url, width=100)
+                with col22:
+                    st.write(aquote)
+                st.info(abiography)
+                   
+
+if masel == 'references':
    with st.container():
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+        st.write("MusicNet dataset: https://lewagon-alumni.slack.com/archives/C05AB0ANML7/p1686750933899749")
 
 if masel == 'Art generation':
    with st.container():
@@ -91,9 +204,16 @@ if masel == 'Art generation':
 if masel == 'Music generation':
    with st.container():
         st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")   
-    
+        tab_options = ["Tab 1", "Tab 2"]
+        selected_tab = st.radio("Select a tab", tab_options)
 
-# Execute your app
+        if selected_tab == "Tab 1":
+            st.markdown("Tab 1 content")
+            # Add your Tab 1 content here
+        elif selected_tab == "Tab 2":
+            st.markdown("Tab 2 content")
+            
+#Execute your app
 #st.title("Javascript example")
 #html(my_html)
 
